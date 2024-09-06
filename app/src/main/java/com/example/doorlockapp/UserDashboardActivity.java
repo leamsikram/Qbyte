34f3bfe3c168e787a7d525380ff1bbd812d2a328
leamsikram <markismaelsoriano>
2024-09-06 16:04:02 +0800

    first commit

---
+++ app/src/main/java/com/example/doorlockapp/UserDashboardActivity.java
@@ -0,0 +1,24 @@
+package com.example.doorlockapp;
+
+import android.os.Bundle;
+import android.widget.Switch;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UserDashboardActivity extends AppCompatActivity {
+    Switch aSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+
+        setContentView(R.layout.activity_user_dashboard);
+        aSwitch = findViewById(R.id.idSwitch);
+    }
+
+    }
